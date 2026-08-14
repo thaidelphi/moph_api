5adc59bba419d09c9ecbce87f8e7c77650918ddc
tewan <tewann.ko@gmail.com>
2026-08-14 16:20:42 +0700

    Eliminate FortiGate portal display by submitting in background iframe and redirecting on completion

--- freepascal/fpsso/src/FortiGate.pas
+++ freepascal/fpsso/src/FortiGate.pas
@@ -138,7 +138,8 @@ begin
     '        <h2>กำลังอนุญาตสิทธิ์เข้าใช้งานอินเทอร์เน็ต</h2>' + LineEnding +
     '        <p>กรุณารอสักครู่ ระบบกำลังลงทะเบียนอุปกรณ์ของท่านกับทาง FortiGate...</p>' + LineEnding +
     '    </div>' + LineEnding +
-    '    <form id="fortigate_form" action="' + HtmlEncode(TargetUrl) + '" method="post" style="display: none;">' + LineEnding +
+    '    <iframe name="fortigate_target_frame" id="fortigate_target_frame" style="display:none; width:0; height:0; border:0;" onload="handleIframeLoaded()"></iframe>' + LineEnding +
+    '    <form id="fortigate_form" target="fortigate_target_frame" action="' + HtmlEncode(TargetUrl) + '" method="post" style="display: none;">' + LineEnding +
     '        <input type="hidden" name="username" value="' + HtmlEncode(Data.Username) + '">' + LineEnding +
     '        <input type="hidden" name="password" value="' + HtmlEncode(Data.PlainPass) + '">' + LineEnding +
     '        <input type="hidden" name="magic" value="' + HtmlEncode(Data.Magic) + '">' + LineEnding +
@@ -148,10 +149,24 @@ begin
     '        <input type="hidden" name="answer" value="1">' + LineEnding +
     '    </form>' + LineEnding +
     '    <script>' + LineEnding +
+    '        var isRedirected = false;' + LineEnding +
+    '        var redirTarget = "' + HtmlEncode(RedirUrl) + '";' + LineEnding +
+    '        function doRedirect() {' + LineEnding +
+    '            if (!isRedirected) {' + LineEnding +
+    '                isRedirected = true;' + LineEnding +
+    '                window.location.replace(redirTarget);' + LineEnding +
+    '            }' + LineEnding +
+    '        }' + LineEnding +
+    '        function handleIframeLoaded() {' + LineEnding +
+    '            setTimeout(doRedirect, 200);' + LineEnding +
+    '        }' + LineEnding +
     '        window.addEventListener("load", function() {' + LineEnding +
-    '            setTimeout(function() {' + LineEnding +
+    '            try {' + LineEnding +
     '                document.getElementById("fortigate_form").submit();' + LineEnding +
-    '            }, 300);' + LineEnding +
+    '            } catch(e) {' + LineEnding +
+    '                console.error("FortiGate submit error:", e);' + LineEnding +
+    '            }' + LineEnding +
+    '            setTimeout(doRedirect, 2000);' + LineEnding +
     '        });' + LineEnding +
     '    </script>' + LineEnding +
     '</body>' + LineEnding +
